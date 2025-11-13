a25b8fa95f3f85eb020136a151b8f89ee6ae7e83
Vena <strom.vena+gh@gmail.com>
2025-11-13 22:00:11 +0100

    WIP renderer

--- Tetris/src/Renderer.cs
+++ Tetris/src/Renderer.cs
@@ -7,6 +7,33 @@ class Renderer
         List<Tetromino> tetrominoes = board.Tetrominoes;
         List<Tetromino> fallingTetrominoes = board.FallingTetrominoes;
 
+        string buffer = "";
+        buffer += """
+        
+            ▄▄▄▄ ▄▄▄ ▄▄▄▄ ▄▄▖ ▗▖ ▗▄▖ 
+             ▐▌  █▄   ▐▌  █▂█ ▐▌ ▀▙▝
+             ▐▌  █▄▄  ▐▌  █▀▙ ▐▌ ▜▄▛
 
+
+        """;
+
+        for (int y = board.VisibleHeight; y < board.Height; y++)
+        {
+            for (int x = 0; x < board.Width; x++)
+            {
+                if (grid[y][x])
+                {
+                    buffer += "#"; // occupied
+                }
+                else
+                {
+                    buffer += "."; // empty
+                }
+            }
+            buffer += "\n";
+        }
+
+        Console.Clear(); // clear and draw to mitigate stutter and general unpleasantries
+        Console.WriteLine(buffer);
     }
 }
