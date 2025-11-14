cbd24363185d2f21bf5789fb09fbdeab57516fa5
lukas-laas <lukas.s.laas@gmail.com>
2025-11-14 15:02:06 +0100

    Tetris instance handles time

--- Tetris/src/Tetris.cs
+++ Tetris/src/Tetris.cs
@@ -6,16 +6,25 @@ class Tetris
     public Board Board { get; } = new();
 
     // Buffs and debuffs
-    public int Dt;
+
     // score
     private int score;
     private int money;
+    private int dt = 500;
+
+    private long lastTick = DateTimeOffset.Now.ToUnixTimeMilliseconds();
     public void Tick()
     {
-        if (Dt < 0)
+        long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        if (currentTime - lastTick > dt)
         {
             Board.Tick();
         }
+    }
+
+    public void SetDt(int dt)
+    {
+        this.dt = dt;
     }
 
     public void Move(Input direction)
