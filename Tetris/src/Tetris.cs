f316fe9702da2468775e8955821afdfaf1d3f47f
lukas-laas <lukas.s.laas@gmail.com>
2025-11-14 14:38:22 +0100

    edit tetris tick

--- Tetris/src/Tetris.cs
+++ Tetris/src/Tetris.cs
@@ -1,11 +1,25 @@
-
 class Tetris
 {
     // Next block(s) and such
-
+    private List<Tetromino> queue = [];
     // board
+    private Board board = new();
 
     // Buffs and debuffs
-
+    public int Dt;
     // score
+    private int score;
+    private int money;
+    public void Tick()
+    {
+        if (Dt < 0)
+        {
+            board.Tick();
+        }
+    }
+
+    public void Move(Input direction)
+    {
+        // Move block
+    }
 }
