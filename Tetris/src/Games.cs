13a9f5c69e8b726d45bbf0af0f03afa0d321606d
lukas-laas <lukas.s.laas@gmail.com>
2025-11-14 13:27:25 +0100

    begin game class

--- Tetris/src/Games.cs
+++ Tetris/src/Games.cs
@@ -3,4 +3,14 @@ class Games
     // overarching game state
 
     // boards via tetris (plural)
+    public Games(int players)
+    {
+        List<Tetris> tetrises = new(players);
+
+        while (true)
+        {
+            string key = Input.Read() ?? "";
+            //Renderer renderer = new(this);
+        }
+    }
 }
