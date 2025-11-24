14ce21aead5c65e1655d71005d181724446e1b18
lukas-laas <lukas.s.laas@gmail.com>
2025-11-24 14:47:31 +0100

    Move logs to utils

---
+++ Tetris/src/util/Log.cs
@@ -0,0 +1,22 @@
+/// <summary>
+/// The most non OOP class ever
+/// </summary>
+static class Log
+{
+    private static readonly string logDirName = "logs";
+    private static readonly string logFileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".log";
+    private static readonly string logFilePath = Path.Join(logDirName, logFileName);
+
+    private static readonly DirectoryInfo _logDirInfo = Directory.CreateDirectory(logDirName); // Ensure log directory exists
+    private static readonly FileStream logFileStream = File.Create(logFilePath);
+
+    public static void Add(string message)
+    {
+        // Append message to log file with timestamp
+        string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        string logMessage = $"[{dateTime}] {message}{Environment.NewLine}";
+        byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(logMessage);
+        logFileStream.Write(messageBytes, 0, messageBytes.Length);
+        logFileStream.Flush();
+    }
+}
