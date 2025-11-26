6d6f71dd1d5105a8ba84de8f93be2bce778a94cd
lukas-laas <lukas.s.laas@gmail.com>
2025-11-26 14:18:14 +0100

    Shop and player stuff

--- Tetris/src/Player.cs
+++ Tetris/src/Player.cs
@@ -3,7 +3,7 @@ class Player
     string name;
     bool isAi;
     int score;
-    int money;
+    public int Money;
     List<IProduct> inventory = [];
     IAbilityProduct? currentAbility;
 
@@ -12,7 +12,7 @@ class Player
         this.name = name;
         isAi = false;
         score = 0;
-        money = 0;
+        Money = 0;
         // currentAbility = new HardDrop(); // TODO: implement
     }
 
@@ -20,7 +20,36 @@ class Player
     {
         if (currentAbility?.Cooldown == 0)
         {
-            // currentAbility.Use(); // TODO: implement
+            currentAbility.Use(); // TODO: implement
         }
+    }
+
+    public void AddToInventory(IProduct product)
+    {
+        if (product is IAbilityProduct temp)
+        {
+            currentAbility = temp;
+        }
+        else
+        {
+            inventory.Add(product);
+            product.Use();
+        }
+    }
+
+    public void TickInventory()
+    {
+        foreach (IProduct product in inventory)
+        {
+            if (product is ITemporaryProduct temp)
+            {
+                temp.UpdateLifetime();
+                if (temp.lifetime == 0)
+                {
+                    temp.Disable();
+                }
+            }
+        }
+        if ((currentAbility != null) && (currentAbility.Cooldown > 0)) currentAbility.Cooldown--;
     }
 }
