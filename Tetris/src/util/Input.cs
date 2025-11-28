dc115c07d6380ccb95b9eb6a4e5f2e5c1029b3ab
VenaStrom <strom.vena+gh@gmail.com>
2025-11-28 15:12:52 +0100

    add sdl2 note in input.cs

--- Tetris/src/util/Input.cs
+++ Tetris/src/util/Input.cs
@@ -1,5 +1,7 @@
 class KeyInput
 {
+    // TODO https://github.com/libsdl-org/SDL
+    
     // TODO add key release requirement to pass along the same key again so holding won't send same key multiple times
     public static string? Read()
     {
