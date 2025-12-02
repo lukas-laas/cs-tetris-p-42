fbf0ac529dd76380babf891826eaa9ad1f4cbbbc
VenaStrom <strom.vena+gh@gmail.com>
2025-12-02 16:24:54 +0100

    remove comments

--- Tetris/src/util/Input.cs
+++ Tetris/src/util/Input.cs
@@ -1,8 +1,5 @@
 class KeyInput
 {
-    // TODO https://github.com/libsdl-org/SDL
-    
-    // TODO add key release requirement to pass along the same key again so holding won't send same key multiple times
     public static string? Read()
     {
         if (Console.KeyAvailable)
