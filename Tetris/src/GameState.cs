4c7878ebfd67591c519d30251159010d864dbc8d
VenaStrom <strom.vena+gh@gmail.com>
2025-12-02 15:45:09 +0100

    add krav 5

--- Tetris/src/GameState.cs
+++ Tetris/src/GameState.cs
@@ -1,6 +1,15 @@
 
 class GameState
 {
+    // KRAV 5:
+    // 1: Beroendeinjektion
+    // 2: Player instansieras med med olika kontrollscheman för att stödja
+    //     flera spelare på samma maskin. Kontrollschemat injiceras in i
+    //     Player-objektet vid konstruktion.
+    // 3: Det är en ren och tydlig implementation som gör det enkelt att
+    //     skapa nya spelare med olika kontroller utan att behöva hårdkoda
+    //     eller ändra i Player-klassen. Det är antingen den här lösningen eller
+    //     att direkt sätta ett fält efter konstruktion vilket är mindre önskvärt.
     public List<Player> Players { get; } = [
         new HumanPlayer("Lukas", new () {
             { "A", Input.Left },
